9f96ef7f8041ba5285dedc1d8fdd4ff70a1ff827
yizhouhe <yizhou.he@gmail.com>
2026-05-31 13:44:16 -0400

    fix1

--- installer.sh
+++ installer.sh
@@ -318,9 +318,9 @@ elif [ "$PLATFORM" = "Darwin" ]; then
         SCRIPT_DIR="\$(cd "\$(dirname "\${BASH_SOURCE[0]}")" && pwd)"
         export IPADDRESS0=$(ipconfig getifaddr en0)
         export IPADDRESS1=$(ipconfig getifaddr en1)
-        if [[ IPADDRESS0 ]]; then
+        if [[ ${IPADDRESS0} ]]; then
             open http://${IPADDRESS0}:18423/
-        elif [[ IPADDRESS1 ]]; then
+        elif [[ ${IPADDRESS1} ]]; then
             open http://"${IPADDRESS1}":18423/
         else
             open http://127.0.0.1:18423/

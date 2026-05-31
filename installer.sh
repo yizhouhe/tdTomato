767388d33664edc79c800fa9bf2bbf9968b38708
yizhouhe <yizhou.he@gmail.com>
2026-05-31 14:01:58 -0400

    fix8

--- installer.sh
+++ installer.sh
@@ -316,14 +316,14 @@ elif [ "$PLATFORM" = "Darwin" ]; then
 #   TOMATO_WEB_ADDR=0.0.0.0:18423
 #   TOMATO_WEB_PASSWORD=你的密码
 SCRIPT_DIR="\$(cd "\$(dirname "\${BASH_SOURCE[0]}")" && pwd)"
-export IPADDRESS0=$(ipconfig getifaddr en0)
-export IPADDRESS1=$(ipconfig getifaddr en1)
-echo $IPADDRESS0
-echo $IPADDRESS1
-if [[ "$IPADDRESS0">"" ]]; then
-    open http://$IPADDRESS0:18423/
-elif [[ "$IPADDRESS1">"" ]]; then
-    open http://"$IPADDRESS1":18423/
+export IPADDRESS0=\$(ipconfig getifaddr en0)
+export IPADDRESS1=\$(ipconfig getifaddr en1)
+echo \$IPADDRESS0
+echo \$IPADDRESS1
+if [[ "\$IPADDRESS0">"" ]]; then
+    open http://\$IPADDRESS0:18423/
+elif [[ "\$IPADDRESS1">"" ]]; then
+    open http://"\$IPADDRESS1":18423/
 else
     open http://127.0.0.1:18423/
 fi
